7277caa9f641776aca766f00d105ff0270e796ef
jRimbault <jacques.rimbault@gmail.com>
2018-07-31 21:45:09 +0200

    Add rsync
    And since it's here now, let's use it

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,13 +2,14 @@
 
 set -euo pipefail
 readonly DIR="$(dirname "$(readlink -e "$0")")"
+readonly BASE_NAME=$(basename "$0")
 
 
 setup_scripts()
 {
   echo "Installing scripts"
   mkdir -p "$HOME/.local/bin"
-  cp "$DIR"/* "$HOME/.local/bin"
+  rsync -aq --exclude "$DIR/$BASE_NAME" "$DIR"/* "$HOME/.local/bin"
   chmod 744 "$HOME/.local/bin"/*
   echo "Done installing scripts"
 }
