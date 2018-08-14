57eb56baf83c18a67e7819e4fba6d5042c1df9a4
jRimbault <jacques.rimbault@gmail.com>
2018-08-14 22:55:05 +0200

    Move git config to dedicated folder

--- rc/setup.sh
+++ rc/setup.sh
@@ -5,15 +5,21 @@ readonly DIR="$(dirname "$(readlink -e "$0")")"
 source "$(dirname "$DIR")/shell/function"
 
 
+git_config()
+{
+  mkdir -p "$HOME/.config/git"
+  soft_force_symlink "$DIR/git/config" "$HOME/.config/git/config"
+  soft_force_symlink "$DIR/git/ignore" "$HOME/.config/git/ignore"
+  soft_force_symlink "$DIR/git/helper.sh" "$HOME/.config/git/helper.sh"
+}
+
 setup_home()
 {
   echo "Setting up the home directory"
   mkdir -p "$HOME/.local/bin"
   mkdir -p "$HOME/.npm"
   soft_force_symlink "$DIR/vim" "$HOME/.vimrc"
-  soft_force_symlink "$DIR/gitconfig" "$HOME/.gitconfig"
-  soft_force_symlink "$DIR/gitignore" "$HOME/.gitignore"
-  soft_force_symlink "$DIR/githelper.sh" "$HOME/.githelper.sh"
+  git_config
   echo "Done setting up the home directory"
 }
 
