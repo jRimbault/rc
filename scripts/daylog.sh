f5bde13561c8172f7bc31227c5df4ea9078ed1c8
jRimbault <jacques.rimbault@gmail.com>
2018-08-19 19:10:47 +0200

    Add daylog script

---
+++ scripts/daylog.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#/ Author: jRimbault
+#/
+#/ Sometimes I want to write a log of the day.
+#/ And I like to have them organized.
+#/
+#/ Options:
+#/   --help, -h display this help
+
+set -euo pipefail
+readonly JOURNAL="$HOME/Documents/Journal"
+
+
+usage()
+{
+  grep "^#/" "$0" | cut -c4-
+  exit 0
+}
+
+main()
+{
+  [ -d "$JOURNAL" ] || mkdir -p "$JOURNAL"
+  local month="$(date +%Y-%m)"
+  local day="$(date +%d)"
+  $EDITOR "$JOURNAL/$month/$day".md
+}
+
+dispatch()
+{
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -h|--help)
+        usage
+        ;;
+    esac
+    shift
+  done
+
+  main
+}
+
+# executes only when executed directly not sourced
+if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
+  dispatch "$@"
+fi
