5024ac3b3714657830820c7aa487b445c2a76384
jRimbault <jacques.rimbault@gmail.com>
2018-09-07 12:42:58 +0200

    For ubuntu

---
+++ packages/add-repos.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# TODO : debian ? (modern, this is only for me)
+# but debian testing (buster) already has up to date packages mostly
+
+set -euo pipefail
+readonly DIR="$(dirname "$(readlink -e "$0")")"
+source "$(dirname "$DIR")/shell/function"
+
+
+declare -gra UBUNTU_PPAS=(
+  ppa:ondrej/php
+  ppa:deadsnakes/ppa
+)
+
+is_ubuntu()
+{
+  source /etc/os-release
+  if [[ "$NAME" == "Ubuntu" ]]; then
+    return 0
+  fi
+  return 1
+}
+
+main()
+{
+  if is_ubuntu; then
+    for ppa in "${UBUNTU_PPAS[@]}"; do
+      sudo add-apt-repository "$ppa"
+      sudo apt-get udpate -q
+    done
+  fi
+}
+
+# do not execute script if it is sourced or downloaded-piped to bash
+if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
+  main "$@"
+fi
