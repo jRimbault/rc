ee215e7e582e5db659b9df19a9ca0989bb052517
jRimbault <jacques.rimbault@gmail.com>
2019-02-03 14:11:19 +0100

    improve padding and speed

--- scripts/scan_for_repos.py
+++ scripts/scan_for_repos.py
@@ -13,14 +13,20 @@ LOOP = asyncio.get_event_loop()
 
 
 async def main(args, argv):
-    tasks = [show_repos(args.dir, repo) for repo in find_repos(args.dir)]
-    for task in tasks:
-        print(await task, flush=True)
+    repos = find_repos(args.dir)
+    tasks = [get_repo_status(os.path.join(args.dir, repo)) for repo in repos]
+    padding = len(max(repos, key=len)) + 2
+    for repo, task in zip(repos, tasks):
+        status = await task
+        print(f"{repo:<{padding}}: {status}", flush=True)
 
 
-async def show_repos(base_dir, repo):
-    status = await get_repo_status(os.path.join(base_dir, repo))
-    return f"{repo:<50}: {status}"
+def find_repos(base_dir):
+    def clean(filename):
+        return filename[len(base_dir) : -4].strip("/")
+
+    search_glob = os.path.join(base_dir, "**/.git")
+    return [clean(f) for f in glob.iglob(search_glob, recursive=True)]
 
 
 def async_t(func):
@@ -31,10 +37,14 @@ def async_t(func):
     return threaded_executor
 
 
+@async_t
+def get_repo_status(repo):
+    gitdir = os.path.join(repo, ".git")
+    command = f"git --git-dir {gitdir} --work-tree {repo} status".split()
+    return message(run(command))
+
+
 class Colors:
-    BOLD = "\033[1m"  # unused
-    UNDERLINE = "\033[4m"  # unused
-    HEADER = "\033[95m"  # unused
     OKBLUE = "\033[94m"  # write operation succeeded
     OKGREEN = "\033[92m"  # readonly operation succeeded
     OKPURPLE = "\033[95m"  # readonly (fetch) operation succeeded
@@ -42,24 +52,9 @@ class Colors:
     FAIL = "\033[91m"  # operation did not succeed
     ENDC = "\033[0m"  # reset color
 
-    # list of all colors
-    ALL = [BOLD, UNDERLINE, HEADER, OKBLUE, OKGREEN, OKPURPLE, WARNING, FAIL, ENDC]
-
-    # map from ASCII to Windows color text attribute
-    WIN_DICT = {
-        BOLD: 15,
-        UNDERLINE: 15,
-        HEADER: 15,
-        OKBLUE: 11,
-        OKGREEN: 10,
-        WARNING: 14,
-        FAIL: 12,
-        ENDC: 15,
-    }
-
 
 def colorize(color, message):
-    return "%s%s%s" % (color, message, Colors.ENDC)
+    return "{0}{1}{2}".format(color, message, Colors.ENDC)
 
 
 def message(out):
@@ -91,15 +86,6 @@ def message(out):
     return ", ".join(messages)
 
 
-@async_t
-def get_repo_status(repo):
-    gitdir = os.path.join(repo, ".git")
-    command = ["git", "--git-dir", gitdir, "--work-tree", repo, "status"]
-    out = run(command)
-
-    return message(out)
-
-
 def run(command):
     try:
         output = subprocess.check_output(command)
@@ -112,21 +98,10 @@ def run(command):
         return e.output
 
 
-def find_repos(base_dir):
-    def clean(filename):
-        return filename[len(base_dir) : -4].strip("/")
-
-    search_glob = os.path.join(base_dir, "**/.git")
-    for filename in glob.iglob(search_glob, recursive=True):
-        yield clean(filename)
-
-
 def parse_args(argv):
     p = argparse.ArgumentParser()
     p.add_argument(
-        "dir",
-        help="directory to parse sub dirs from. (default: .)",
-        default=".",
+        "dir", help="directory to parse sub dirs from. (default: .)", default="."
     )
     return p.parse_known_args(argv)
 
