99075211202425f971bc2fa0777b84185ede1178
jRimbault <jacques.rimbault@gmail.com>
2019-02-04 22:06:10 +0100

    pull

--- scripts/cluster-git.py
+++ scripts/cluster-git.py
@@ -42,6 +42,8 @@ async def main(args, argv):
         await loop(repos_statuses)
     elif args.fetch:
         await loop(repos_fetch)
+    elif args.pull:
+        await loop(repos_pull)
     else:
         print(*[clean(repo) for repo in repos], sep="\n")
 
@@ -54,12 +56,20 @@ async def repos_statuses(repos):
 
 
 async def repos_fetch(repos):
-    git_status = git_repo("status")
+    git_status = run_in_repo("status")
     git_fetch = as_completed(git_repo(["fetch", "--all"]))
     for task in git_fetch(repos):
         errcode, repo, out = await task
-        _, _, status = await git_status(repo)
+        _, status = git_status(repo)
         yield repo, status_message(status) + ", " + fetch_message(errcode, out)
+
+
+async def repos_pull(repos):
+    git_pull = as_completed(git_repo("pull"))
+    for task in git_pull(repos):
+        out = "No action taken"
+        _, repo, out = await task
+        yield repo, pull_message(out)
 
 
 def as_completed(method):
@@ -82,27 +92,29 @@ def async_io(func):
     return threaded_executor
 
 
-def git_repo(command):
+def git_repo(action):
     @async_io
     def inner(repo):
-        def run_git_repo(action):
-            command = [
-                "git",
-                "--git-dir",
-                os.path.join(repo, ".git"),
-                "--work-tree",
-                repo,
-            ]
-            if type(action) == list:
-                command += action
-            elif type(action) == str:
-                command.append(action)
-            return run(command)
-
-        status, out = run_git_repo(command)
+        status, out = run_git_repo(repo, action)
         return status, repo, out
 
     return inner
+
+
+def run_in_repo(action):
+    def inner(repo):
+        return run_git_repo(repo, action)
+
+    return inner
+
+
+def run_git_repo(repo, action):
+    command = ["git", "--git-dir", os.path.join(repo, ".git"), "--work-tree", repo]
+    if type(action) == list:
+        command += action
+    elif type(action) == str:
+        command.append(action)
+    return run(command)
 
 
 def fetch_message(errcode, out):
@@ -145,6 +157,27 @@ def status_message(out):
     return ", ".join(messages)
 
 
+def pull_message(out):
+    if out == "No action taken":
+        return out
+
+    messages = []
+    if re.search(r"Already up.to.date", out):
+        messages.append(colorize(Colors.OKGREEN, "Pulled nothing"))
+    elif "CONFLICT" in out:
+        messages.append(colorize(Colors.FAIL, "Pull conflict"))
+    elif "fatal: No remote repository specified." in out:
+        messages.append(colorize(Colors.WARNING, "Pull remote not configured"))
+    elif "fatal: " in out:
+        messages.append(colorize(Colors.FAIL, "Pull fatal"))
+    elif "error: " in out:
+        messages.append(colorize(Colors.FAIL, "Pull error"))
+    else:
+        messages.append(colorize(Colors.OKBLUE, "Pulled"))
+
+    return ", ".join(messages)
+
+
 class Colors:
     OKBLUE = "\033[94m"  # write operation succeeded
     OKGREEN = "\033[92m"  # readonly operation succeeded
@@ -179,11 +212,10 @@ def parse_args(argv):
         # If there are no changes it can also push and pull to/from a remote location.
     )
     parser.add_argument("dir", help="directory to parse sub dirs from")
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument(
-        "-s", "--status", help="show repository status", action="store_true"
-    )
-    group.add_argument("-f", "--fetch", help="fetch from remote", action="store_true")
+    actions = parser.add_mutually_exclusive_group()
+    actions.add_argument("-s", "--status", help="show status", action="store_true")
+    actions.add_argument("-f", "--fetch", help="fetch from remote", action="store_true")
+    actions.add_argument("-p", "--pull", help="pull from remote", action="store_true")
     parser.add_argument(
         "-A", "--absolute", help="display absolute paths", action="store_true"
     )
