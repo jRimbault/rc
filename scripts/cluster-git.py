29325b49feddf946cff3005ab8b4a8a7b56a7dd5
jRimbault <jacques.rimbault@gmail.com>
2019-02-05 21:01:13 +0100

    add some comment

--- scripts/cluster-git.py
+++ scripts/cluster-git.py
@@ -32,6 +32,7 @@ async def main(args, argv):
         print(f"{clean(repo):<{padding}}: {status}", flush=True)
 
     async def loop(method):
+        """ helper method to keep all actions under one interface """
         async for repo, status in method(repos):
             _print(repo, status)
 
@@ -54,14 +55,14 @@ async def repos_statuses(repos):
     git_status = iter_as_completed(git_async_action("status"))
     for task in git_status(repos):
         _, repo, out = await task
-        yield repo, status_message(out)
+        yield repo, Parse.status_message(out)
 
 
 async def repos_fetch(repos):
     git_async_status = git_async_action("status")
     git_fetch = iter_as_completed(git_async_action(["fetch", "--all"]))
 
-    for task in git_status(git_fetch(repos), git_async_status, fetch_message):
+    for task in git_status(git_fetch(repos), git_async_status, Parse.fetch_message):
         yield await task
 
 
@@ -69,7 +70,7 @@ async def repos_pull(repos):
     git_async_status = git_async_action("status")
     git_pull = iter_as_completed(git_async_action("pull"))
 
-    for task in git_status(git_pull(repos), git_async_status, pull_message):
+    for task in git_status(git_pull(repos), git_async_status, Parse.pull_message):
         yield await task
 
 
@@ -77,11 +78,17 @@ async def repos_push(repos):
     git_async_status = git_async_action("status")
     git_push = iter_as_completed(git_async_action("push"))
 
-    for task in git_status(git_push(repos), git_async_status, push_message):
+    for task in git_status(git_push(repos), git_async_status, Parse.push_message):
         yield await task
 
 
 def iter_as_completed(method):
+    """
+    transforms an async function taking one item into a list procecessing function
+    yielding its result in completion order (first finished first yielded)
+    """
+
+    @wraps(method)
     def inner(iterable, *a, **kw):
         return asyncio.as_completed([method(i, *a, **kw) for i in iterable])
 
@@ -90,9 +97,13 @@ def iter_as_completed(method):
 
 @iter_as_completed
 async def git_status(task, git_async_status, extract_message):
+    """
+    expects the result from another git_async_action
+    allows them chaining together and keep yielding in completion order
+    """
     errcode, repo, out = await task
     _, _, status = await git_async_status(repo)
-    messages = [extract_message(errcode, out), status_message(status)]
+    messages = [extract_message(errcode, out), Parse.status_message(status)]
     return repo, ", ".join(messages)
 
 
@@ -102,6 +113,11 @@ def find_repos(base_dir):
 
 
 def async_io(func):
+    """
+    Transforms any io function in the standard libraries to be used in async mode
+    by wrapping them in thread, but using async/await syntax.
+    """
+
     @wraps(func)
     def threaded_executor(*args):
         return LOOP.run_in_executor(None, func, *args)
@@ -110,8 +126,11 @@ def async_io(func):
 
 
 def git_async_action(action):
+    """ Run a git command in async """
+
     @async_io
     def inner(repo):
+        """ on the given repository """
         status, out = run_git_repo(repo, action)
         return status, repo, out
 
@@ -119,6 +138,7 @@ def git_async_action(action):
 
 
 def run_git_repo(repo, action):
+    """ Run a git command on the given repository """
     command = ["git", "--git-dir", os.path.join(repo, ".git"), "--work-tree", repo]
     if type(action) == list:
         command += action
@@ -127,86 +147,87 @@ def run_git_repo(repo, action):
     return run(command)
 
 
-def fetch_message(errcode, out):
-    fetch = []
-    if "error: " in out:
-        fetch.append(colorize(Colors.FAIL, "Fetch fatal"))
-    else:
-        fetch.append(colorize(Colors.OKPURPLE, "Fetched"))
-    if errcode != 0:
-        fetch.append(colorize(Colors.FAIL, "Fetch unsuccessful"))
-    return ", ".join(fetch)
+class Parse:
+    """ Parse a user readable message from the stdout/stderr of git """
 
+    @staticmethod
+    def fetch_message(errcode, out):
+        fetch = []
+        if "error: " in out:
+            fetch.append(colorize(Colors.FAIL, "Fetch fatal"))
+        else:
+            fetch.append(colorize(Colors.OKPURPLE, "Fetched"))
+        if errcode != 0:
+            fetch.append(colorize(Colors.FAIL, "Fetch unsuccessful"))
+        return ", ".join(fetch)
 
-def status_message(out):
-    messages = []
-    clean = True
-    if "On branch master" not in out:
-        branch = out.splitlines()[0].replace("On branch ", "")
-        messages.append(colorize(Colors.WARNING, "On branch %s" % branch))
-        clean = False
-    # changed from "directory" to "tree" in git 2.9.1
-    # https://github.com/mnagel/clustergit/issues/18
-    if re.search(r"nothing to commit.?.?working (directory|tree) clean.?", out):
-        messages.append(colorize(Colors.OKBLUE, "No Changes"))
-    elif "nothing added to commit but untracked files present" in out:
-        messages.append(colorize(Colors.WARNING, "Untracked files"))
-        clean = False
-    else:
-        messages.append(colorize(Colors.FAIL, "Changes"))
-        clean = False
-    if "Your branch is ahead of" in out:
-        messages.append(colorize(Colors.FAIL, "Unpushed commits"))
-        clean = False
+    @staticmethod
+    def status_message(out):
+        messages = []
+        clean = True
+        if "On branch master" not in out:
+            branch = out.splitlines()[0].replace("On branch ", "")
+            messages.append(colorize(Colors.WARNING, "On branch %s" % branch))
+            clean = False
+        # changed from "directory" to "tree" in git 2.9.1
+        # https://github.com/mnagel/clustergit/issues/18
+        if re.search(r"nothing to commit.?.?working (directory|tree) clean.?", out):
+            messages.append(colorize(Colors.OKBLUE, "No Changes"))
+        elif "nothing added to commit but untracked files present" in out:
+            messages.append(colorize(Colors.WARNING, "Untracked files"))
+            clean = False
+        else:
+            messages.append(colorize(Colors.FAIL, "Changes"))
+            clean = False
+        if "Your branch is ahead of" in out:
+            messages.append(colorize(Colors.FAIL, "Unpushed commits"))
+            clean = False
 
-    if clean:
-        messages = [colorize(Colors.OKGREEN, "Clean")]
+        if clean:
+            messages = [colorize(Colors.OKGREEN, "Clean")]
 
-    return ", ".join(messages)
+        return ", ".join(messages)
 
+    @staticmethod
+    def pull_message(errcode, out):
+        messages = []
+        if re.search(r"Already up.to.date", out):
+            messages.append(colorize(Colors.OKGREEN, "Pulled nothing"))
+        elif "CONFLICT" in out:
+            messages.append(colorize(Colors.FAIL, "Pull conflict"))
+        elif "fatal: No remote repository specified." in out:
+            messages.append(colorize(Colors.WARNING, "Pull remote not configured"))
+        elif "fatal: " in out:
+            messages.append(colorize(Colors.FAIL, "Pull fatal"))
+        elif "error: " in out:
+            messages.append(colorize(Colors.FAIL, "Pull error"))
+        else:
+            messages.append(colorize(Colors.OKBLUE, "Pulled"))
 
-def pull_message(errcode, out):
-    if out == "No action taken":
-        return out
+        return ", ".join(messages)
 
-    messages = []
-    if re.search(r"Already up.to.date", out):
-        messages.append(colorize(Colors.OKGREEN, "Pulled nothing"))
-    elif "CONFLICT" in out:
-        messages.append(colorize(Colors.FAIL, "Pull conflict"))
-    elif "fatal: No remote repository specified." in out:
-        messages.append(colorize(Colors.WARNING, "Pull remote not configured"))
-    elif "fatal: " in out:
-        messages.append(colorize(Colors.FAIL, "Pull fatal"))
-    elif "error: " in out:
-        messages.append(colorize(Colors.FAIL, "Pull error"))
-    else:
-        messages.append(colorize(Colors.OKBLUE, "Pulled"))
+    @staticmethod
+    def push_message(errcode, out):
+        messages = []
+        error = False
+        if "read-only" in out:
+            messages.append(colorize(Colors.WARNING, "Read-only remote"))
+            error = True
+        if re.search(r"\[(remote )?rejected\]", out):
+            messages.append(colorize(Colors.FAIL, "Push rejected"))
+            error = True
+        if "denied" in out:
+            messages.append(colorize(Colors.FAIL, "Permission denied"))
+            error = True
 
-    return ", ".join(messages)
+        if "Everything up-to-date" in out:
+            messages.append(colorize(Colors.OKGREEN, "Already up-to-date"))
+            error = True
 
+        if not error:
+            messages.append(colorize(Colors.OKBLUE, "Push accepted"))
 
-def push_message(errcode, out):
-    messages = []
-    error = False
-    if "read-only" in out:
-        messages.append(colorize(Colors.WARNING, "Read-only remote"))
-        error = True
-    if re.search(r"\[(remote )?rejected\]", out):
-        messages.append(colorize(Colors.FAIL, "Push rejected"))
-        error = True
-    if "denied" in out:
-        messages.append(colorize(Colors.FAIL, "Permission denied"))
-        error = True
-
-    if "Everything up-to-date" in out:
-        messages.append(colorize(Colors.OKGREEN, "Already up-to-date"))
-        error = True
-
-    if not error:
-        messages.append(colorize(Colors.OKBLUE, "Push accepted"))
-
-    return ", ".join(messages)
+        return ", ".join(messages)
 
 
 class Colors:
