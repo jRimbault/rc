f2a2915e27805d30176168547c66fa882b55df28
jRimbault <jacques.rimbault@gmail.com>
2020-08-17 18:50:16 +0200

    try to add my usual git aliases to pwsh
    powershell: "Look at me! I'm a shell!"
    me: "You can't even define aliases."
    powershell: "But I can use all of .NET!"
    me: "..."
    powershell: "I have objects! Structured programming! Automatic documentation!"
    me: "can you define an alias?"

--- shell/profile.ps1
+++ shell/profile.ps1
@@ -47,14 +47,34 @@ function Set-MyPSReadLineOptions {
     Set-PSReadlineKeyHandler -Chord Ctrl+RightArrow -Function ShellForwardWord
 }
 
+
+function g { git $args }
+function ga { git add $args }
+function gau { git add --update $args }
+function gb { git branch $args }
+function gcw { git commit -v $args }
+function gc! { git commit -v --amend $args }
+function gcb { git checkout -b $args }
+function gcl { git clone --recurse-submodules -j8 $args }
+function gco { git checkout $args }
+function gd { git diff $args }
+function gdw { git diff --word-diff $args }
+function gf { git fetch -v --all $args }
+function gpl { git pull $args }
+function gph { git push $args }
+function gpd { git push --dry-run $args }
+function gss { git status -s $args }
+function gst { git status $args }
+function gwch { git whatchanged -p --abbrev-commit --pretty=medium $args }
+function gcurrent { git rev-parse HEAD $args }
+
 $hosts = @(
     'Visual Studio Code Host',
     'ConsoleHost'
 )
 
 if ($hosts.Contains($host.Name)) {
+    Invoke-Expression (&starship init powershell)
     Set-MyPSReadLineOptions
-    Import-Module posh-git
-    $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
 }
 
